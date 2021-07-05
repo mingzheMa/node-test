cd359e038b45a7414cf90d0053746195eead427a
marx <marx10086@gmail.com>
2021-07-05 13:51:55 +0800

    client cache

--- routers/index.js
+++ routers/index.js
@@ -75,9 +75,33 @@ app.use(
   })
 );
 
-
 // 静态资源中间件
-app.use(express.static(path.resolve(__dirname, "../client/dist")));
+// 首次请求静态页面，服务器响应响应头：
+// Cache-Control: public, max-age=31536000 // public代表资源是公开的，max-age代表资源缓存时间为31536000秒
+// Date: Mon, 05 Jul 2021 05:32:26 GMT // 请求返回的时间，用来计算缓存是否过期
+// ETag: W/"40785-17a74c2030a" // 资源编号，如果服务器资源发生改变，则该资源编号也会随着改变，w/代表不区分大小写
+// Last-Modified: Mon, 05 Jul 2021 03:41:49 GMT // 最后操作的时间
+
+// 浏览器接收到响应：
+//   如果响应头中有Cache-Control.max-age或者Last-Modified，则缓存
+//   请求方法，请求路径，过期时间，接口返回时间，资源编号，最后操作时间
+//   如果没有，则不缓存
+
+// 浏览器后续请求时会先去缓存中找资源编号对应的资源
+//   如果没有对应资源则发送请求(和首次请求一致)
+//   如果有资源编号，找到该资源并查看是否过期
+//     如果过期则发出请求询问服务器改请求是否可以使用缓存，服务器根据以下请求头判断资源是否改变
+//     If-None-Match: W/"121-171ca289ebf" // 资源编号
+//       如果资源改变，则返回200并附加请求体
+//       如果资源未改变，则返回304并附加响应头(和首次请求的响应头一致)表示资源可以继续使用缓存
+//     如果没有过期，则继续使用缓存中的资源
+
+
+app.use(
+  express.static(path.resolve(__dirname, "../client/dist"), {
+    maxAge: 1000 * 3600 * 24 * 365, // 静态资源缓存一年，因为每次变动jscss资源的hash值都会变化，所以不用担心代码变动浏览器读取缓存
+  })
+);
 // 图片防盗链
 app.use("/public/img", antiTheftLinkMiddleware);
 app.use("/public", express.static(path.resolve(__dirname, "../public")));
